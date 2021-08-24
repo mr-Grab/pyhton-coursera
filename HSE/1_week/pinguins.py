8a6a1b1e70a0f3222217b20a07dc4e473ce4ef72
Bychkov Nikita <nikitoskin.2012@gmail.com>
2021-08-24 18:00:24 +0300

    fix e225 and w391

--- HSE/1_week/pinguins.py
+++ HSE/1_week/pinguins.py
@@ -1,39 +1,13 @@
-str1 = '   _~_   '
-str2 = '  (o o)  '
-str3 = ' /  V  \ '
-str4 = "/(  _  )\"
-str5 = "  ^^ ^^  "
+str1 = '    _~_    '
+str2 = '   (o o)   '
+str3 = '  /  V  \  '
+str4 = ' /(  _  )\ '
+str5 = '   ^^ ^^   '
 
 countPinguins = int(input())
-getCount = countPinguins
 
-while(countPinguins > 0):
-    print(str1, end = ' ')
-    countPinguins -= 1
-countPinguins = getCount
-print('')
-
-while(countPinguins > 0):
-    print(str2, end = ' ')
-    countPinguins -= 1
-countPinguins = getCount
-print('')
-
-while(countPinguins > 0):
-    print(str3, end = ' ')
-    countPinguins -= 1
-countPinguins = getCount
-print('')
-
-while(countPinguins > 0):
-    print(str4, end = ' ')
-    countPinguins -= 1
-countPinguins = getCount
-print('')
-
-while(countPinguins > 0):
-    print(str5, end = ' ')
-    countPinguins -= 1
-countPinguins = getCount
-print('')
-
+print(str1 * countPinguins)
+print(str2 * countPinguins)
+print(str3 * countPinguins)
+print(str4 * countPinguins)
+print(str5 * countPinguins)
