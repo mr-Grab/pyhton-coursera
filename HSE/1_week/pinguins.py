f775df159bcb13ab68fd55f94db81276f59a9cd3
Bychkov Nikita <nikitoskin.2012@gmail.com>
2021-08-24 17:40:36 +0300

    remove pinguin's ending spaces and move their to print function

--- HSE/1_week/pinguins.py
+++ HSE/1_week/pinguins.py
@@ -1,38 +1,38 @@
-str1 = '   _~_    '
-str2 = '  (o o)   '
-str3 = ' /  V  \  '
-str4 = "/(  _  )\ "
-str5 = "  ^^ ^^   "
+str1 = '   _~_   '
+str2 = '  (o o)  '
+str3 = ' /  V  \ '
+str4 = "/(  _  )\"
+str5 = "  ^^ ^^  "
 
 countPinguins = int(input())
 getCount = countPinguins
 
 while(countPinguins > 0):
-    print(str1, end = '')
+    print(str1, end = ' ')
     countPinguins -= 1
 countPinguins = getCount
 print('')
 
 while(countPinguins > 0):
-    print(str2, end = '')
+    print(str2, end = ' ')
     countPinguins -= 1
 countPinguins = getCount
 print('')
 
 while(countPinguins > 0):
-    print(str3, end = '')
+    print(str3, end = ' ')
     countPinguins -= 1
 countPinguins = getCount
 print('')
 
 while(countPinguins > 0):
-    print(str4, end = '')
+    print(str4, end = ' ')
     countPinguins -= 1
 countPinguins = getCount
 print('')
 
 while(countPinguins > 0):
-    print(str5, end = '')
+    print(str5, end = ' ')
     countPinguins -= 1
 countPinguins = getCount
 print('')
