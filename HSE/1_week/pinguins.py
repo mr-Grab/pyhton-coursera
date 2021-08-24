0ab397576da2fd75c46e0f196c67072891d43385
Bychkov Nikita <nikitoskin.2012@gmail.com>
2021-08-24 17:23:09 +0300

    version 1.0

--- HSE/1_week/pinguins.py
+++ HSE/1_week/pinguins.py
@@ -1,7 +1,39 @@
 str1 = '   _~_    '
 str2 = '  (o o)   '
 str3 = ' /  V  \  '
-str4 = '/(  _  )\ '
-str5 = '  ^^ ^^   '
+str4 = "/(  _  )\ "
+str5 = "  ^^ ^^   "
 
 countPinguins = int(input())
+getCount = countPinguins
+
+while(countPinguins > 0):
+    print(str1, end = '')
+    countPinguins -= 1
+countPinguins = getCount
+print('')
+
+while(countPinguins > 0):
+    print(str2, end = '')
+    countPinguins -= 1
+countPinguins = getCount
+print('')
+
+while(countPinguins > 0):
+    print(str3, end = '')
+    countPinguins -= 1
+countPinguins = getCount
+print('')
+
+while(countPinguins > 0):
+    print(str4, end = '')
+    countPinguins -= 1
+countPinguins = getCount
+print('')
+
+while(countPinguins > 0):
+    print(str5, end = '')
+    countPinguins -= 1
+countPinguins = getCount
+print('')
+
