c211d48584d5aca35ab02a35795b5fd6df2300c3
Bychkov Nikita <nikitoskin.2012@gmail.com>
2021-08-26 19:00:23 +0300

    fix w191

--- HSE/1_week/sum_of_digits.py
+++ HSE/1_week/sum_of_digits.py
@@ -3,8 +3,8 @@ numb = int(input())
 iter = 3
 sum = 0
 while (iter > 0):
-	dig = numb % 10
-	numb = numb // 10
-	sum = sum + dig
-	iter = iter - 1
+  dig = numb % 10
+  numb = numb // 10
+  sum = sum + dig
+  iter = iter - 1
 print(sum)
