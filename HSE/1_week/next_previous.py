159cfcbe4ca72e15594762b76b9db9c0f9ebdf7a
Bychkov Nikita <nikitoskin.2012@gmail.com>
2021-08-27 14:43:36 +0300

    next/prev numb case

---
+++ HSE/1_week/next_previous.py
@@ -0,0 +1,6 @@
+numb = int(input())
+
+print('The next number for the number ', numb,
+      ' is ', numb + 1, '.', sep='')
+print('The previous number for the number ', numb,
+      ' is ', numb - 1, '.', sep='')
